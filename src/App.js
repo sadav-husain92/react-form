ee2534f97ced22c340fd147e0a0e7c6afcc129b3
sadav-husain92 <sadav.husain92@gmail.com>
2020-07-05 17:31:56 +0530

    Registration-Form-Reactjs

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import RegistrationForm from './RegistrationForm/RegistrationForm';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <RegistrationForm />
+   
+    </div>
+  );
+}
+
+export default App;
